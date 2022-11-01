7677ab25e18933feada81b5ec2ee8efe4ead62a6
Florina <ffiador@gmail.com>
2022-11-01 20:17:57 +0000

    react.1

---
+++ react-webpage/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function ClickableButton() {
+
+  const [clicked, setClick] = React.useState(false);
+
+  if (clicked) {
+    return 'Nothing here yet! Change this text to add more info.';
+  }
+
+  return React.createElement(
+    'button', {
+    className : 'main-button',
+
+    onClick: () => setClick(true)
+  },
+    'Click To Reveal'
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(React.createElement(ClickableButton));
