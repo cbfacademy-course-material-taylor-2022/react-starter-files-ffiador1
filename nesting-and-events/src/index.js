8c4799986d1131aa35b1131ab43d46cbab8434c5
Florina <ffiador@gmail.com>
2022-11-17 20:05:56 +0000

    nesting and events, second task

--- nesting-and-events/src/index.js
+++ nesting-and-events/src/index.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import TaskContainer from './components/TaskContainer';
-/*
+import Task from './components/Task';
+
 const taskList = [
   {"id":"task1","title":"Buy Milk","isComplete":true},
   {"id":"task2","title":"Read a book","isComplete":false},
@@ -10,19 +11,30 @@ const taskList = [
   {"id":"task5","title":"Visit the puppy shelter","isComplete":true},
   {"id":"task6","title":"Watch Bad Sisters","isComplete":false}
 ];
-*/
+
 
 function App() {
   
+  
+    function toggleComplete(id){
+      console.log(`Task with the id '${id}' was clicked`);
+    }
    return (
+    
       <Fragment>
         <h1 key="heading">Task List</h1>
         <TaskContainer>
           {/* Task: Nesting Components - Add you tasks here: */}
+          {taskList.sort((a, b) => (a.isComplete - b.isComplete))
+          .map(task => <Task toggleComplete={toggleComplete} 
+          id={task.id} title={task.title} isComplete={task.isComplete}/>)}
+       
         </TaskContainer>
       </Fragment>
   );
+
 }
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
